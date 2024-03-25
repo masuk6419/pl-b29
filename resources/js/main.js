aa950ff6254af0f9d56ec9801483cbc2e6758b79
Masuk Hussain <masuk4386@gmail.com>
2024-03-26 05:31:51 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,14 @@
+// NAVIGATION
+
+
+function openMenu(){
+    document.getElementById("menu-container").style.width = "100%";
+}
+
+function closeMenu(){
+    document.getElementById("menu-container").style.width = "0";
+}
+
+// MIXITUP 
+
+var mixer = mixitup('.work-grid');
